2ebd4c8754fa834473b2fa1e3ac915705ccf9a9b
Alexisraj <80027669+Alexisraj@users.noreply.github.com>
2021-05-05 21:28:53 +0800

    style fixes

--- src/components/section/DetailedSection.js
+++ src/components/section/DetailedSection.js
@@ -1,32 +1,62 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import PropTypes from "prop-types";
 
 const DetailedSection = (props) => {
-  let videUrl = "./video/Beach.mp4";
-  if (props.thunderstorm) videUrl = "./video/Thunderstorm.mp4";
+  // let videUrl = "./video/Beach.mp4";
+  // if (props.thunderstorm) videUrl = "./video/Thunderstorm.mp4";
   return (
     <div>
       <a className="close" href="#" onClick={() => props.closePopup()}>
         <img src="./images/close.png" alt="close" />
       </a>
-      <span>{props.selectedDayMin}</span>
-      <span>{props.selectedDayMax}</span>
-      <span>{props.selectedDay}</span>
-      <span>{props.thunderstorm}</span>
-      <span>{props.canBeRain}</span>
-      <span>{props.cloudDenses}</span>
       <div>
-        <i className={`wi ${props.weatherIcon} display-4`} />
-        {props.thunderstorm && (
-          <video
-            width="200px"
-            height="200px"
-            src={videUrl}
-            muted
-            loop
-            autoPlay
-          />
-        )}
+        <h4 className="Header">The detailed Report for {props.selectedDay}</h4>
+        <div className="tableSection">
+          <table class="tg">
+            <thead>
+              <tr>
+                <th class="tg-2fdn">Day</th>
+                <th class="tg-0lax">
+                  <span>{props.selectedDay}</span>
+                </th>
+                <th class="tg-0lax" colspan="2">
+                  <i className={`wi ${props.weatherIcon} display-4`} />
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td class="tg-0lax">Max</td>
+                <td class="tg-0lax">
+                  <span>{props.selectedDayMax}&deg;</span>
+                </td>
+                <td class="tg-0lax">Min</td>
+                <td class="tg-0lax">
+                  <span>{props.selectedDayMin}&deg;</span>
+                </td>
+              </tr>
+              <tr>
+                <td class="tg-0lax">Rain</td>
+                <td class="tg-0lax">
+                  <span>{props.canBeRain}</span>
+                </td>
+                <td class="tg-0lax">Thunderstorm</td>
+                <td class="tg-0lax">
+                  <span>{props.thunderstorm}</span>
+                </td>
+              </tr>
+              <tr>
+                <td class="tg-0lax">Cloud</td>
+                <td class="tg-0lax">
+                  <span>{props.cloudDenses}</span>
+                </td>
+                <td class="tg-0lax">Fog</td>
+                <td class="tg-0lax">No</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
